b9ef3452dc6703e98f07fa37b7a7189629c85fa7
miki <mikinagai616@gmail.com>
2013-10-30 19:09:48 -0400

    admin features

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,27 @@ $(window).load(function() {
 		$('#slider').nivoSlider();
 });
 
+AJAX = {};
+
+AJAX.login = function() {
+    var ajaxOptions = {
+    data: {test: '1'},
+    dataType: 'html',
+    error: function(response) {
+        alert('AJAX error');
+    },
+    success: function(response) {
+    	window.location = 'confirmLogin.php?userQuery=' + userQuery + '&passwordQuery=' + passwordQuery;
+    },
+    type: 'GET',
+    url: 'confirmLogin.php?userQuery=' + userQuery + '&passwordQuery=' + passwordQuery
+    };
+    $.ajax(ajaxOptions);
+};
+
+
 $(document).ready(function(){
+
 	$('#box-products-wrapper').carouFredSel({
 					auto: false,
 					prev: '#prev2',
@@ -26,5 +46,13 @@ $(document).ready(function(){
 			}, function() {
 		$(this).attr("src","img/Twitter.png");
 	});
+
+	$('body').on('click','#loginSubmit', function(e) {
+                   userQuery = $('input#userName').val();
+                   passwordQuery = $('input#password').val();
+                   AJAX.login();
+
+        });
     
+
 });
